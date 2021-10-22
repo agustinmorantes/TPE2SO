b4dff6f68fe57e426c5af285916c475ec7a6d9c3
jnegroITBA <jnegro@itba.edu.ar>
2021-10-21 23:11:59 -0300

    Better MemAlloc based on Heap4

--- Shared/memory_manager.c
+++ Shared/memory_manager.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stddef.h>
 
-#define MAX_SIZE 64*1024*1024
+#define MAX_SIZE 1048576 // 1Mb
 #define START_MM 0x900000
+#define BYTE_ALIGMENT 8
+#define ALIGMENT_MASK 0x0007
+#define MINIMUM_BLOCK_SIZE (( size_t ) structSize << 1)
+#define BITS_PER_BYTE (( size_t ) 8)
 
-static char * current;
-
-void * alloc(size_t size) 
+typedef struct freeNode 
 {
-    if (current == 0)
-        current = START_MM;
+    struct freeNode * next;
+    size_t nodeSize;
+} freeNode;
+
+static freeNode start;
+static freeNode * end = NULL;
+static const size_t nodeSize = ( sizeof( freeNode ) + ( ( size_t ) ( BYTE_ALIGMENT - 1 ) ) ) & ~( ( size_t ) ALIGMENT_MASK );
+static size_t freeBytesRemaining = 0U;
+static size_t nodeAllocatedBit = 0;
+
+static void addNodeToFreeList(freeNode toAdd);
+
+static void listInit() 
+{
+    freeNode * first;
+    uint8_t alignedHeap;
+    size_t startAddress = (size_t) START_MM;
+    size_t totalHeapSize = (size_t) MAX_SIZE;
+
+    if ((startAddress & ALIGMENT_MASK) != 0 )
+    {
+        startAddress += (BYTE_ALIGMENT - 1);
+        startAddress &= ~((size_t) ALIGMENT_MASK);
+        totalHeapSize -= startAddress - (size_t)START_MM;
+    }
+
+
     
-    size = size%16 ? size + (16 - size%16) : size;
-
-    if (current + size > START_MM + MAX_SIZE)
-        return NULL;
-
-    void * aux = current;
-    current += size;
-    return aux;
 }
 
-void free(void * ptr)
-{
-    return;
-}
